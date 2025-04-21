7cd5f9bee1c43d3b59fd5447c1fd68b204a99dc6
BlazeTitan000 <volodymyr.andrash5576@ukr.net>
2025-04-22 02:16:09 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from app.routes import main_routes, image_routes, video_routes
 from app.utils.config import Config
 from app.services.model_service import preload_models
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+                template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'),
+                static_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'))
     
     # Load configuration
     app.config.from_object(Config)
